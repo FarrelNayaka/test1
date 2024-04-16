8420109cbb64d168dbe7bca5267ab0679fe2606d
FarrelNayaka <163793989+FarrelNayaka@users.noreply.github.com>
2024-04-16 20:19:10 +0700

    Add files via upload

---
+++ contacts.js
@@ -0,0 +1,21 @@
+export const contacts = [
+    {
+      name: "Beyonce",
+      imgURL: "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg",
+      phone: "+123 456 7898",
+      email: "b@beyonce.com"
+    },
+    {
+      name: "Jack Bauer",
+      imgURL: "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
+      phone: "+987 654 3210",
+      email: "jack@nowhere.com"
+    },
+    {
+      name: "Chuck Norris",
+      imgURL: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fswiatseriali.interia.pl%2Fplotki%2Fnews-chuck-norris-wycofal-sie-z-aktorstwa-nie-moze-jednak-narzeka%2CnId%2C7339393&psig=AOvVaw1KRgMNqxVlY12RtvVYiLd1&ust=1713359716351000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCMDstbzoxoUDFQAAAAAdAAAAABAR",
+      phone: "+918 372 5744",
+      email:"gmail@chucknorris.com"
+    }
+  ];
+  
